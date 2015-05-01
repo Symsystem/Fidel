fdd34419977345f89a535e75905156443ca6bcb7
jeremyduchesne <duchesnejeremy@gmail.com>
2015-05-01 12:48:31 +0200

    Debut ConnectActivity

---
+++ app/src/main/java/com/fidel/fidel/ConnectActivity.java
@@ -0,0 +1,18 @@
+package com.fidel.fidel;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class ConnectActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_connect);
+    }
+
+
+}
