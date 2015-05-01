db1eeef7a5e43a430645713c7213851a1e70ebbc
Symsystem <symeon.delmarmol@gmail.com>
2015-05-01 10:57:52 +0200

    Début mainActivity

--- app/src/main/java/com/fidel/fidel/MainActivity.java
+++ app/src/main/java/com/fidel/fidel/MainActivity.java
@@ -2,15 +2,21 @@ package com.fidel.fidel;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // coucou je m'appelle Quentin :D
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
     }
 }
