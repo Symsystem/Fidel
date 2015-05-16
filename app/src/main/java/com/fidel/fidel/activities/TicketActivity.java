bbacd69291a0a59f491149641db7c07a7c4c2c03
Symsystem <symeon.delmarmol@gmail.com>
2015-05-16 19:53:23 +0200

    Ticket activity ! :)

--- app/src/main/java/com/fidel/fidel/activities/TicketActivity.java
+++ app/src/main/java/com/fidel/fidel/activities/TicketActivity.java
@@ -1,17 +1,25 @@
 package com.fidel.fidel.activities;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ListView;
+import android.widget.ExpandableListView;
 import android.widget.TextView;
 
 import com.fidel.fidel.R;
 import com.fidel.fidel.adapters.PersonnesAdapter;
+import com.fidel.fidel.classes.Personne;
 import com.fidel.fidel.classes.Reservation;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -21,12 +29,12 @@ public class TicketActivity extends ActionBarActivity implements AdapterView.OnI
     Toolbar toolbar;
     private Reservation mReservation;
 
-    @InjectView(R.id.numResId) TextView mNumRes;
-    @InjectView(R.id.dateId) TextView mDate;
-    @InjectView(R.id.typeVoyageurId) TextView mTypeVoyageur;
-    @InjectView(R.id.typeVolId) TextView mTypeVol;
+    @InjectView(R.id.numResContent) TextView mNumResContent;
+    @InjectView(R.id.dateContent) TextView mDateContent;
+    @InjectView(R.id.typeVoyageurContent) TextView mTypeVoyageurContent;
+    @InjectView(R.id.typeVolContent) TextView mTypeVolContent;
 
-    @InjectView(R.id.listPersonnes) ListView mListPersonne;
+    @InjectView(R.id.listPersonnes) ExpandableListView mListPersonne;
     @InjectView(android.R.id.empty)TextView empty;
 
     @Override
@@ -43,13 +51,35 @@ public class TicketActivity extends ActionBarActivity implements AdapterView.OnI
         Intent intent = getIntent();
         mReservation = (Reservation)intent.getSerializableExtra("reservation");
 
-        mNumRes.setText("Réservation N° : " + mReservation.getNumRes());
-        mDate.setText("Date de réservation : " + mReservation.getDate());
+        Date dateRes = null;
+        try {
+            dateRes = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'+'SSSS").parse(mReservation.getDate());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        String formattedDateRes = new SimpleDateFormat("dd / MM / yyyy").format(dateRes);
 
-        mTypeVoyageur.setText("Billet(s) de classe " + mReservation.getTypeVoyageur());
-        mTypeVol.setText(("Vol de type " + mReservation.getVol().getTypeVol()));
 
-        PersonnesAdapter adapter = new PersonnesAdapter(this, mReservation.getListPersonne());
+        mNumResContent.setText(String.valueOf(mReservation.getNumRes()));
+        mDateContent.setText(formattedDateRes);
+
+        mTypeVoyageurContent.setText(String.valueOf(mReservation.getTypeVoyageur()));
+        mTypeVolContent.setText(String.valueOf(mReservation.getVol().getTypeVol()));
+
+        ArrayList<Personne> listPersonnes = mReservation.getListPersonne();
+        ArrayList<String> listNomsPersonnes = new ArrayList<>();
+
+        for(int i = 0; i<listPersonnes.size(); i++){
+            listNomsPersonnes.add(listPersonnes.get(i).getPrenom() + " " + listPersonnes.get(i).getNom());
+        }
+
+        mListPersonne.setDividerHeight(2);
+        mListPersonne.setGroupIndicator(null);
+        mListPersonne.setClickable(true);
+
+        PersonnesAdapter adapter = new PersonnesAdapter(this,
+                (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE),
+                listNomsPersonnes, listPersonnes);
         mListPersonne.setAdapter(adapter);
         mListPersonne.setEmptyView(empty);
         mListPersonne.setOnItemClickListener(this);
--- app/src/main/java/com/fidel/fidel/adapters/PersonnesAdapter.java
+++ app/src/main/java/com/fidel/fidel/adapters/PersonnesAdapter.java
@@ -4,38 +4,45 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.CheckedTextView;
 import android.widget.TextView;
 
 import com.fidel.fidel.R;
 import com.fidel.fidel.classes.Personne;
 
-import java.util.List;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 /**
  * Created by sym on 3/05/15.
  */
-public class PersonnesAdapter extends ArrayAdapter<Personne>{
+public class PersonnesAdapter extends BaseExpandableListAdapter {
 
     private Context mContext;
-    private List<Personne> mPersonneList;
+    private ArrayList<String> mParentItems;
+    private ArrayList<Personne> mChildItems;
 
-    public PersonnesAdapter(Context context, List<Personne> objects) {
-        super(context, R.layout.pers_list_item, objects);
+    private LayoutInflater inflater;
 
-        mContext = context;
-        mPersonneList = objects;
+    public PersonnesAdapter(Context context, LayoutInflater inflater, ArrayList<String> parentItems, ArrayList<Personne> childItems){
+        mParentItems = parentItems;
+        mChildItems = childItems;
+        this.inflater = inflater;
+        this.mContext = context;
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getChildView(int groupPosition, final int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
 
         ViewHolder holder;
 
         if (convertView == null) {
+
             convertView = LayoutInflater.from(mContext).inflate(R.layout.pers_list_item, null);
             holder = new ViewHolder();
-            holder.name = (TextView) convertView.findViewById(R.id.pers_name);
             holder.address = (TextView) convertView.findViewById(R.id.pers_adress);
             holder.phoneNumber = (TextView) convertView.findViewById(R.id.pers_phoneNumber);
             holder.birthDate = (TextView) convertView.findViewById(R.id.pers_birthDate);
@@ -48,22 +55,95 @@ public class PersonnesAdapter extends ArrayAdapter<Personne>{
             holder = (ViewHolder) convertView.getTag();
         }
 
-        Personne personne = mPersonneList.get(position);
+        Personne personne = mChildItems.get(groupPosition);
 
-        holder.name.setText("Nom : " + personne.getPrenom() + " " + personne.getNom());
-        holder.address.setText(("Adresse : " + personne.getAddress() + System.getProperty("line.separator")
-                + personne.getPostCode() + personne.getLocality() + System.getProperty("line.separator")
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'+'SSSS");
+        Date birth = null;
+        Date passeportDate = null;
+        try {
+            birth = sdf.parse(personne.getBirthDate());
+            passeportDate = sdf.parse(personne.getPasseportValidity());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        String formattedBirth = new SimpleDateFormat("dd / MM / yyyy").format(birth);
+        String formattedPasseportDate = new SimpleDateFormat("dd / MM / yyyy").format(passeportDate);
+
+        holder.address.setText(("Adresse : " + System.getProperty("line.separator")
+                + personne.getAddress() + System.getProperty("line.separator")
+                + personne.getPostCode() + " " + personne.getLocality() + System.getProperty("line.separator")
                 + personne.getCountry()));
         holder.phoneNumber.setText("N° de téléphone : " + personne.getNumPhone());
-        holder.birthDate.setText("Date de naissance : " + personne.getBirthDate());
-        holder.passeportValidity.setText("Passeport valide jusqu'au " + personne.getPasseportValidity());
+        holder.birthDate.setText("Date de naissance : " + formattedBirth);
+        holder.passeportValidity.setText("Passeport valide jusqu'au " + formattedPasseportDate);
 
         return convertView;
+    }
 
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.pers_name_item, null);
+        }
+
+        ((CheckedTextView) convertView).setText(mParentItems.get(groupPosition));
+        ((CheckedTextView) convertView).setChecked(isExpanded);
+
+        return convertView;
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        return null;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return 0;
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return 1;
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        return null;
+    }
+
+    @Override
+    public int getGroupCount() {
+        return mParentItems.size();
+    }
+
+    @Override
+    public void onGroupCollapsed(int groupPosition) {
+        super.onGroupCollapsed(groupPosition);
+    }
+
+    @Override
+    public void onGroupExpanded(int groupPosition) {
+        super.onGroupExpanded(groupPosition);
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return 0;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return false;
     }
 
     private static class ViewHolder{
-        TextView name;
         TextView address;
         TextView phoneNumber;
         TextView birthDate;
